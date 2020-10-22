3fa487cf0afe6ff4e9e5cda9a1f7b48993adab72
KarlitosTaquitos <72091668+KarlitosTaquitos@users.noreply.github.com>
2020-10-22 01:18:58 -0500

    Login
    added the logic to log in, doesn't have a new activity to go to after that

--- app/src/main/java/com/example/mombucks/MainActivity.java
+++ app/src/main/java/com/example/mombucks/MainActivity.java
@@ -5,12 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
     ConnectionClass connection;
     EditText user, pass;
     Button register;
+    Button login;
     ProgressDialog progressDialog;
 
     @Override
@@ -28,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
         user = (EditText) findViewById(R.id.username);
         pass = (EditText) findViewById(R.id.password);
         register = (Button) findViewById(R.id.button);
+        login = (Button) findViewById(R.id.button2);
 
         connection = new ConnectionClass();
         progressDialog = new ProgressDialog(this);
@@ -37,6 +41,14 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v){
                 registerBtn doRegister = new registerBtn();
                 doRegister.execute("");
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginBtn doLogin = new loginBtn();
+                doLogin.execute("");
             }
         });
     }
@@ -89,5 +101,55 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    public class loginBtn extends AsyncTask<String, String, String> {
+        String userStr = user.getText().toString();
+        String passStr = pass.getText().toString();
+        String z = "";
+        boolean isSuccess = false;
 
+        @Override
+        protected void onPreExecute() {
+            progressDialog.setMessage("Loading...");
+            progressDialog.show();
+        }
+
+        @Override
+        protected String doInBackground(String... string) {
+            if (userStr.trim().equals("") || passStr.trim().equals(""))
+                z = "Please enter all fields";
+            else {
+                try {
+                    Connection con = connection.CONN();
+                    if (con == null)
+                        z = "Please check your internet connection";
+                    else {
+                        String query = "select * from users where username = '" + userStr + "';";
+                        Log.e("QUERY", query);
+                        Statement stmt = con.createStatement();
+                        ResultSet rs = stmt.executeQuery(query);
+
+                        rs.first();
+                        Log.e("PASSWORD", rs.getString("password"));
+                        if (rs.getString("password").equals(passStr))
+                            z = "Login Successful";
+                        isSuccess = true;
+                    }
+                } catch (Exception e) {
+                    isSuccess = false;
+                    z = "Exception " + e;
+                    Log.e("ERROR", z);
+                }
+            }
+
+            return z;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            if (isSuccess)
+                Toast.makeText(getBaseContext(), "" + z, Toast.LENGTH_LONG).show();
+
+            progressDialog.hide();
+        }
+    }
 }
