134bd50884b511fddec8e3a67c6bbc2d701648f4
Vanessa <70288550+vanessathefirst@users.noreply.github.com>
2020-10-29 21:58:12 -0500

    Create ConnectionClass2.java

---
+++ app/src/main/java/com/myapplication/ConnectionClass2.java
@@ -0,0 +1,42 @@
+package com.mombuck.myapplication;
+
+import android.annotation.SuppressLint;
+import android.os.StrictMode;
+import android.util.Log;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionClass {
+
+    String classs = "com.mysql.jdbc.Driver";
+
+    String url = "jdbc:mysql://34.121.163.145:3306/test";
+    String usnm = "root";
+    String pswd = "MommyMoney";
+
+    @SuppressLint("NewApi")
+    public Connection CONN() {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+
+        StrictMode.setThreadPolicy(policy);
+
+        Connection conn = null;
+        String ConnURL = null;
+
+        try {
+            Class.forName(classs);
+
+            conn = DriverManager.getConnection(url, usnm, pswd);
+
+            //conn = DriverManager.getConnection(ConnURL);
+        } catch (SQLException se) {
+            Log.e("ERRO", se.getMessage());
+        } catch (Exception e) {
+            Log.e("ERRO2", e.getMessage());
+        }
+
+        return conn;
+    }
+}
