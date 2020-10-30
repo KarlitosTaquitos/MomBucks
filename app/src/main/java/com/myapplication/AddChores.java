ab9e30a964707af49b7091eda298e0ac181d9e48
Vanessa <70288550+vanessathefirst@users.noreply.github.com>
2020-10-29 21:58:20 -0500

    Create AddChores.java

---
+++ app/src/main/java/com/myapplication/AddChores.java
@@ -0,0 +1,14 @@
+package com.mombuck.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddChores extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authentication);
+    }
+}
